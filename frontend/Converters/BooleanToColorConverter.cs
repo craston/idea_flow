c9926635e585c2975b6d7ef64d1d9281f74266d9
craston <nievescrasto18@gmail.com>
2024-12-03 01:03:59 -0600

    feat(frontend) - added BooleanToColorConverter to dynamically change message background color based on user message status; updated BrainstormIdeaPage to utilize the converter.

---
+++ frontend/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace frontend.Converters
+{
+    using System;
+    using System.Globalization;
+    using Microsoft.Maui.Graphics;
+
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isUserMessage)
+            {
+                return isUserMessage ? Colors.LightBlue : Colors.LightGray;
+            }
+            return Colors.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
