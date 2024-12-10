e630ebac3318a7918996799f026c6eb1963c6a3e
craston <nievescrasto18@gmail.com>
2024-12-10 05:13:08 -0600

    feat(backend) - added new test prompts with history support and updated output format instructions.

--- backend/prompts/test.py
+++ backend/prompts/test.py
@@ -7,3 +7,14 @@ TEST_PROMPT = """
     The output must be provided as JSON object with the following schema:
     {format_instructions}
     """
+
+TEST_PROMPT_WITH_HISTORY = """
+    You are helpful assistant.
+    Using the following history, please answer the following question in two sentences or less.
+
+    History = {history}
+    Question = {question}
+
+    The output must be provided as JSON object with the following schema:
+    {format_instructions}
+    """
