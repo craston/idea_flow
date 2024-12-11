9751bff19210513ec2ac6d9794edae0933281caf
craston <nievescrasto18@gmail.com>
2024-12-11 00:07:58 -0600

    refactor(frontend) - formatting.

--- frontend/Models/Test.cs
+++ frontend/Models/Test.cs
@@ -1,14 +1,13 @@
 ﻿
 
-namespace frontend.Models
-{
-    public class TestInput
-    {
-        public required string Question { get; set; }
-    }
+namespace frontend.Models;
 
-    public class  TestOutput
-    {
-        public required string Answer { get; set; }
-    }
+public class TestInput
+{
+    public required string Question { get; set; }
+}
+
+public class  TestOutput
+{
+    public required string Answer { get; set; }
 }
