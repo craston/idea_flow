5649bd7f799a800af5f7a38980dc7f144e2090a8
craston <nievescrasto18@gmail.com>
2024-12-26 07:26:35 -0600

    feat(frontend) - integrate SQLite database for idea storage and update App initialization logic.

--- frontend/App.xaml.cs
+++ frontend/App.xaml.cs
@@ -1,12 +1,22 @@
-﻿namespace frontend
-{
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+﻿using frontend.Models;
+namespace frontend;
 
-            MainPage = new AppShell();
+public partial class App : Application
+{
+    static IdeaDatabase database;
+
+    public static IdeaDatabase Database
+    {
+        get
+        {
+            database ??= new IdeaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Ideas.db3"));
+            return database;
         }
+    }
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
     }
 }
